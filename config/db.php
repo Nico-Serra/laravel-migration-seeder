af747950cb407746bd43d45f6f29ac6fbe2fc82a
Nico-Serra <nico.serra000@gmail.com>
2024-05-13 12:03:38 +0200

    add file database with seeder and add new file config db

---
+++ config/db.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    'trains' => [
+        [
+            'azienda' => 'TrenItalia',
+            'stazione_di_partenza' => 'Bari Centrale',
+            'stazione_di_arrivo' => 'Torino Porta Nuova',
+            'orario_di_partenza' => '08:40',
+            'orario_di_arrivo' => '18:40',
+            'data' => '2024-05-13',
+            'codice_treno' => '#56184scs',
+            'in_orario' => 1,
+            'cancellato' => 0,
+        ],
+        [
+            'azienda' => 'TrenItalia',
+            'stazione_di_partenza' => 'Bari Centrale',
+            'stazione_di_arrivo' => 'Milano',
+            'orario_di_partenza' => '08:40',
+            'orario_di_arrivo' => '19:40',
+            'data' => '2024-05-14',
+            'codice_treno' => '#561d852',
+            'in_orario' => 0,
+            'cancellato' => 1,
+        ],
+        [
+            'azienda' => 'Italo',
+            'stazione_di_partenza' => 'Torino Porta Nuova',
+            'stazione_di_arrivo' => 'Milano',
+            'orario_di_partenza' => '16:40',
+            'orario_di_arrivo' => '18:40',
+            'data' => '2024-05-15',
+            'codice_treno' => '#56184ocs',
+            'in_orario' => 1,
+            'cancellato' => 0,
+        ],
+        [
+            'azienda' => 'FrecciaRossa',
+            'stazione_di_partenza' => 'Roma Termini',
+            'stazione_di_arrivo' => 'Torino Porta Nuova',
+            'orario_di_partenza' => '11:40',
+            'orario_di_arrivo' => '18:40',
+            'data' => '2024-05-16',
+            'codice_treno' => '#5618iscs',
+            'in_orario' => 1,
+            'cancellato' => 0,
+        ],
+    ],
+];
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $trains = config('db.trains');
+
+
+        foreach ($trains as $train) {
+            $newTrain = new Train();
+            $newTrain->azienda = $train['azienda'];
+            $newTrain->stazione_di_partenza = $train['stazione_di_partenza'];
+            $newTrain->stazione_di_arrivo = $train['stazione_di_arrivo'];
+            $newTrain->orario_di_partenza = $train['orario_di_partenza'];
+            $newTrain->orario_di_arrivo = $train['orario_di_arrivo'];
+            $newTrain->data = $train['data'];
+            $newTrain->codice_treno = $train['codice_treno'];
+            $newTrain->in_orario = $train['in_orario'];
+            $newTrain->cancellato = $train['cancellato'];
+            $newTrain->save();
+        }
+    }
+}
